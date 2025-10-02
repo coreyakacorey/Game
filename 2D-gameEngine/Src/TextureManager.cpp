c5ce40ea8a01171b8b7508dfc53e933237ca7493
coreyakacorey <48829912+coreyakacorey@users.noreply.github.com>
2025-10-02 09:22:54 +0900

    Update TextureManager.cpp

--- 2D-gameEngine/Src/TextureManager.cpp
+++ 2D-gameEngine/Src/TextureManager.cpp
@@ -3,9 +3,12 @@
 SDL_Texture* TextureManager::LoadTexture(const char* texture) {
 
 	SDL_Surface* tempSurface = IMG_Load(texture);
-	SDL_Texture* text = SDL_CreateTextureFromSurface(Game::renderer, tempSurface);
+	SDL_Texture* tex = SDL_CreateTextureFromSurface(Game::renderer, tempSurface);
 	SDL_FreeSurface(tempSurface);
 
-	return text;
+	return tex;
+}
 
+void TextureManager::Draw(SDL_Texturer* tex, SDL_Rect sourcePosisiton, SDL_Rect destinationPosisiton){
+	SDL_RenderCopy(Game::renderer, tex, &sourcePosisiton, &destinationPosisiton);
 }
