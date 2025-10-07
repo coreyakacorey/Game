257909e57a897f2ed3df39d0ffdd290b2aac4a54
coreyakacorey <48829912+coreyakacorey@users.noreply.github.com>
2025-10-07 10:37:14 +0900

    Update TileComponent.h

--- 2D-gameEngine/Src/ECS/TileComponent.h
+++ 2D-gameEngine/Src/ECS/TileComponent.h
@@ -6,6 +6,7 @@ class TileComponent : public Component {
 public:
 	SDL_Texture* texture;
 	SDL_Rect srcRect, destRect;
+	Vector2D position;
 
 	TileComponent() = default;
 
@@ -16,6 +17,9 @@ public:
 	TileComponent(int srcX, int srcY, int xpos, int ypos, const char* path) {
 		texture = TextureManager::LoadTexture(path);
 
+		position.x = xpos;
+		position.y = ypos;
+		
 		srcRect.x = srcX;
 		srcRect.y = srcY;
 		srcRect.w = srcRect.h = 32;
@@ -23,6 +27,11 @@ public:
 		destRect.x = xpos;
 		destRect.y = ypos;
 		destRect.w = destRect.h = 64;
+	}
+
+	void update() override{
+		destRect.x = position.x - Game::camera.x;
+		destRect.y = position.y - Game::camera.y;
 	}
 
 	void draw() override{
