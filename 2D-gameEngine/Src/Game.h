4a0783ca937daaf8217c6cce6c6a08e1f2e05930
coreyakacorey <48829912+coreyakacorey@users.noreply.github.com>
2025-10-08 09:18:12 +0900

    Update Game.h

--- 2D-gameEngine/Src/Game.h
+++ 2D-gameEngine/Src/Game.h
@@ -23,7 +23,7 @@ public:
 
 	bool running() { return isRunning;  }
 
-	static void AddTile(int srcX,int srcY, int xpos, int ypos);
+	
 	static SDL_Renderer* renderer;
 	static SDL_Event event;
 	static std::vector<ColliderComponent*> colliders;
@@ -38,6 +38,7 @@ private:
 
 
 #endif /* Game_h */
+
 
 
 
