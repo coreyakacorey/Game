5c40e7345dadc53534b2ad32f74348d656301536
coreyakacorey <48829912+coreyakacorey@users.noreply.github.com>
2025-10-08 09:15:39 +0900

    Update ColliderComponent.h

--- 2D-gameEngine/Src/ECS/ColliderComponent.h
+++ 2D-gameEngine/Src/ECS/ColliderComponent.h
@@ -2,11 +2,15 @@
 #include <string>
 #include "SDL.h"
 #include "Components.h"
+#include "../TextureManager.h"
 
 class ColliderComponent : public Component {
 public:
 	SDL_Rect collider;
 	std::string tag;
+
+SDL_Texture* tex;
+SDL_Rect srcR, destR;
 
 	TransformComponent* transform;
 
@@ -20,13 +24,25 @@ public:
 		}
 		transform = &entity->getComponent<TransformComponent>();
 
-		Game::colliders.push_back(this);
+		tex = TextureManager::LoadTexture("assests/colTex.png");
+		srcR = {0, 0, 32, 32};
+		destR = {collider.x, collider.y, collider.w, collider.h};
 	}
 
 	void update() override {
-		collider.x = static_cast<int>(transform->position.x);
-		collider.y = static_cast<int>(transform->position.y);
-		collider.w = transform->width * transform->scale;
-		collider.h = transform->height * transform->scale;
+		if(tag != "terrain"){
+			collider.x = static_cast<int>(transform->position.x);
+			collider.y = static_cast<int>(transform->position.y);
+			collider.w = transform->width * transform->scale;
+			collider.h = transform->height * transform->scale;
+		}
+
+		//update position due to camera follow
+		destR.x = collider.x - Game::camera.x;
+		destR.y = collider.y - Game::camera.y;
 	}
+
+void draw() override{
+	TextureManager::Draw(tex, srcR, destR, SDL_FLIP_NONE);
+}
 };
